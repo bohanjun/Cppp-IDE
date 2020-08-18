881af5d7b9a7b5513c3ccff66a3674487dece29c
23786 <yixuanzhuzhuzhu123@163.com>
2020-08-18 13:09:14 +0800

    Add shiftLeft(_:) and shiftRight(_:) function

--- C+++/Classes/Edit/CDCodeEditor Extension.swift
+++ C+++/Classes/Edit/CDCodeEditor Extension.swift
@@ -41,6 +41,51 @@ extension CDCodeEditor {
             }
             lineRange.location += line.count + 1
         }
+        self.setSelectedRange(range)
+        self.didChangeText()
+        
+    }
+    
+    @IBAction func shiftRight(_ sender: Any?) {
+        
+        var range = self.selectedRange
+        var lineRange = NSMakeRange(0, 0)
+        for line in self.string.components(separatedBy: .newlines) {
+            lineRange.length = line.count + 1
+            let anotherRange = NSIntersectionRange(range, lineRange)
+            if anotherRange.length != 0 {
+                self.string = self.string.nsString.replacingCharacters(in: NSMakeRange(lineRange.location, lineRange.length - 1), with: "\t" + line)
+                lineRange.location += line.count + 2
+                range.length += 1
+                continue
+            }
+            lineRange.location += line.count + 1
+        }
+        self.setSelectedRange(range)
+        self.didChangeText()
+        
+    }
+    
+    @IBAction func shiftLeft(_ sender: Any?) {
+        
+        var range = self.selectedRange
+        var lineRange = NSMakeRange(0, 0)
+        for line in self.string.components(separatedBy: .newlines) {
+            lineRange.length = line.count + 1
+            let anotherRange = NSIntersectionRange(range, lineRange)
+            if anotherRange.length != 0 {
+                if line.hasPrefix("\t") {
+                    self.string = self.string.nsString.replacingCharacters(in: NSMakeRange(lineRange.location, lineRange.length - 1), with: String(line.dropFirst()))
+                    lineRange.location += line.count
+                    range.length -= 1
+                } else {
+                    lineRange.location += line.count + 1
+                }
+                continue
+            }
+            lineRange.location += line.count + 1
+        }
+        self.setSelectedRange(range)
         self.didChangeText()
         
     }
